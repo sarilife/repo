29328e7f172022c329c0353fbbf1a7f13517b5d3
Ahmad <hmada.kh@gmail.com>
2019-01-21 19:25:13 +0200

    design

--- src/app/advertisement/advertisement.component.ts
+++ src/app/advertisement/advertisement.component.ts
@@ -10,13 +10,10 @@ import { IImage } from 'ng-simple-slideshow';
 })
 export class AdvertisementComponent implements OnInit {
     imageUrls: (string | IImage)[] = [
-        // { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56748793/dbohn_170625_1801_0018.0.0.jpg', caption: 'The first slide', href: '#config' },
-        // { url: 'https://cdn.vox-cdn.com/uploads/chorus_asset/file/9278671/jbareham_170917_2000_0124.jpg', clickAction: () => alert('custom click function') },
-        // { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56789263/akrales_170919_1976_0104.0.jpg', caption: 'Apple TV', href: 'https://www.apple.com/' },
-        // 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56674755/mr_pb_is_the_best.0.jpg',
-        // { url: 'assets/kitties.jpg', backgroundSize: 'contain', backgroundPosition: 'center' }
+
     ];
-    height: string = '400px';
+
+    height: string = '600px';
     minHeight: string;
     arrowSize: string = '30px';
     showArrows: boolean = true;
@@ -46,11 +43,11 @@ export class AdvertisementComponent implements OnInit {
     ngOnInit() {
 
         this.advertisementService.advertisementAsyncGet().subscribe(advertisements => {
-            // this.advertisements = advertisements;
+            this.advertisements = advertisements;
 
             advertisements.forEach(element => {
                 this.imageUrls.push(
-                    { url: String(element.link), caption: 'The first slide', href: '#config' },
+                    { url: String(element.link), caption:element.name + "  " + element.description  , href: '#config' },
                 )
             });
 
