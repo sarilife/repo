99edddae031ab966af1bf6ddb9794e924aa0f790
Ahmad <hmada.kh@gmail.com>
2019-01-22 02:34:15 +0200

    --

--- src/app/bid-item/bid-item.component.ts
+++ src/app/bid-item/bid-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BidItem } from '../shared/models/biditem.model';
 import { BidItemService } from '../shared/services/biditem.service';
+import { SellerService } from '../shared/services/seller.service';
+import { Seller } from '../shared/models/seller.models';
 
 @Component({
     selector: 'app-bid-item',
@@ -9,22 +11,34 @@ import { BidItemService } from '../shared/services/biditem.service';
 })
 export class BidItemComponent implements OnInit {
 
-
+    isValid : boolean = false;
     public bidItem: BidItem[];
-    constructor(public bidItemService: BidItemService) {
+    public sellers: Seller[];
+    constructor(public sellerService: SellerService,public bidItemService: BidItemService) {
 
     }
 
     ngOnInit() {
 
+        this.bidItemService.bidItemAsyncGet().subscribe((e)=>{
+            console.log(e[0].fixed_price )
+            if(e[0].fixed_price == 0){
+                this.isValid = true;
+            }
+        });
+
+        this.sellerService.sellerAsyncGet().subscribe(sellers => {
+            this.sellers = sellers;
+        })
+
         this.bidItemService.bidItemAsyncGet().subscribe(bidItem => {
             this.bidItem = bidItem;
 
+        })
 
-
-        }
-        )
     }
+
+
 
 
 }
